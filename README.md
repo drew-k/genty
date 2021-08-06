762f446834a5ee64041f3b41f7300bd1929ee2d3
Drew Kinney <66277127+TheKinster1@users.noreply.github.com>
2021-08-06 08:26:52 -0400

    Basic Functionality
    Setup basic chat parses, basic structure for the help command, fixed prefix json issues.

--- README.md
+++ README.md
@@ -9,3 +9,10 @@ For Genty's safety, the token is stored in a file ".env" which is accessed at ru
 
 Genty stores individual prefixes for each guild in a json file. Upon joining a guild, Genty sets the default prefix as '%'. This can be changed later and will be updated in the bots memory. When Genty is removed from a guild, it will wipe its memory of a guild's prefix.
 
+## Modmail [IN DEVELOPMENT]
+
+In the future, Genty will be able to help mods easily keep track of threads created by users about server issues.
+
+## Custom VCs [IN DEVELOPEMENT]
+
+While still in developement, Genty will soon be able to handle creating custom voice channels for users upon demand, with deletion when their use is complete.
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import discord
 from discord.ext import commands
 import os
 import json
+import random
 
 def findPrefix(client, message):
     with open ("prefixes.json", "r") as f:
@@ -9,23 +10,37 @@ def findPrefix(client, message):
     return prefixes[str(message.guild.id)]
 
 client = commands.Bot(
-    command_prefix = "%",
+    command_prefix = (findPrefix),
     help_command=None
 )
 
+@client.command(name="help", pass_context=True)
+async def _help(ctx):
+    await ctx.message.delete()
+    embed = discord.Embed(
+        title = "Help",
+        description = "test",
+        color = discord.Color.blue()
+    )
+    await ctx.author.send(embed=embed)
+
 @client.event
 async def on_ready():
+    f=open("prefixes.json", "a+")
     print(f"Bot online as {client.user}.")
 
 @client.event
 async def on_guild_join(guild):
     with open ("prefixes.json", "r") as f:
-        prefixes = json.load(f)
+        try:
+            prefixes = json.load(f)
+        except:
+            prefixes = {}
 
     prefixes[str(guild.id)] = "%"
 
     with open ("prefixes.json", "w") as f:
-        json.dumps(prefixes, f, indent=4)
+        json.dump(prefixes, f, indent=4)
 
 @client.event
 async def on_guild_remove(guild):
@@ -35,6 +50,19 @@ async def on_guild_remove(guild):
     prefixes.pop(str(guild.id))
 
     with open ("prefixes.json", "w") as f:
-        json.dumps(prefixes, f, indent=4)
+        json.dump(prefixes, f, indent=4)
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith("say hi"):
+        if random.randint(0,3) > 0:
+            await message.channel.send("hi")
+        else:
+            await message.channel.send("leave me alone")
+
+    await client.process_commands(message)
 
 client.run(os.getenv("TOKEN"))
