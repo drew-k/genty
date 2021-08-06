a27a20eb148328210dcdd4ba74dfb27c1556be7b
Drew Kinney <66277127+TheKinster1@users.noreply.github.com>
2021-08-06 00:53:56 -0400

    Initial Prefix Setup
    -Default prefix on server join in .json\n-Pop server prefix on guild remove\n-Bot parsing json for guild prefix on startup

--- main.py
+++ main.py
@@ -1,10 +1,40 @@
 import discord
 from discord.ext import commands
 import os
+import json
+
+def findPrefix(client, message):
+    with open ("prefixes.json", "r") as f:
+        prefixes = json.load(f)
+    return prefixes[str(message.guild.id)]
 
 client = commands.Bot(
     command_prefix = "%",
     help_command=None
 )
+
+@client.event
+async def on_ready():
+    print(f"Bot online as {client.user}.")
+
+@client.event
+async def on_guild_join(guild):
+    with open ("prefixes.json", "r") as f:
+        prefixes = json.load(f)
+
+    prefixes[str(guild.id)] = "%"
+
+    with open ("prefixes.json", "w") as f:
+        json.dumps(prefixes, f, indent=4)
+
+@client.event
+async def on_guild_remove(guild):
+    with open ("prefixes.json", "r") as f:
+        prefixes = json.load(f)
+
+    prefixes.pop(str(guild.id))
+
+    with open ("prefixes.json", "w") as f:
+        json.dumps(prefixes, f, indent=4)
 
 client.run(os.getenv("TOKEN"))
