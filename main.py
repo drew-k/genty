f61040c1a7fb1c6fe8d0d0ba5c9c5887b0a37d9d
Drew Kinney <66277127+TheKinster1@users.noreply.github.com>
2021-08-09 19:27:45 -0400

    Created the custom VC setup command
    When the setup command is called, the bot creates a category and voice channel that houses and creates custom VCs, respectively. The bot stores the IDs of these channels in the prefixes JSON for later reference.

--- main.py
+++ main.py
@@ -4,12 +4,10 @@ import os
 import json
 import random
 
-_id = 873565797887389696
-
 def findPrefix(client, message):
     with open ("prefixes.json", "r") as f:
         prefixes = json.load(f)
-    return prefixes[str(message.guild.id)]
+    return prefixes[str(message.guild.id)][0]
 
 client = commands.Bot(
     command_prefix = (findPrefix),
@@ -25,6 +23,24 @@ async def _help(ctx):
         color = discord.Color.blue()
     )
     await ctx.author.send(embed=embed)
+
+@client.command(name="setup", pass_context=True)
+async def _vcsetup(ctx):
+    custom_category = await ctx.guild.create_category(
+        name="Custom Voice Channels",
+        position=1)
+    create_channel = await ctx.guild.create_voice_channel(
+        name = "Create Custom Channel",
+        category = custom_category,
+        position = 0)
+
+    with open ("prefixes.json", "r") as f:
+        prefixes = json.load(f)
+
+    prefixes[str(ctx.guild.id)] = [prefixes[str(ctx.guild.id)], custom_category.id, create_channel.id]
+
+    with open ("prefixes.json", "w") as f:
+        json.dump(prefixes, f, indent=4)
 
 @client.command(name="name", pass_context=True)
 async def _name(ctx, nm: str=None):
@@ -63,13 +79,17 @@ async def _unlock(ctx):
 
 @client.event
 async def on_voice_state_update(member, before, after):
-    custom_channel_category = client.get_channel(873294766916386817)
+    with open ("prefixes.json", "r") as f:
+        prefixes = json.load(f)
+    custom_category = client.get_channel(id=prefixes[str(member.guild.id)][1])
+    create_channel = client.get_channel(id=prefixes[str(member.guild.id)][2])
+
     # Creates VCs
     if not before.channel and after.channel:
-        if after.channel.id == _id:
+        if after.channel.id == create_channel.id:
             new_channel = await member.guild.create_voice_channel(
                 name=f"{member.display_name}\'s Channel",
-                category=custom_channel_category,
+                category=custom_category,
                 position = 1)
             await member.move_to(new_channel)
             with open ("channels.json", "r") as f:
