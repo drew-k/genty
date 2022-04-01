d7eb36a9fefbc15b4afce940dfcb1ded97599945
TheKinster1 <66277127+TheKinster1@users.noreply.github.com>
2022-04-01 13:25:26 -0400

    Added guild count command.

--- src/extensions/slash_commands.py
+++ src/extensions/slash_commands.py
@@ -4,49 +4,55 @@ from datetime import datetime
 
 
 class SlashCommands(commands.Cog):
-  def __init__(self, bot):
-    self.bot:commands.Bot = bot
+    def __init__(self, bot):
+        self.bot:commands.Bot = bot
 
-  @commands.slash_command(description="Unload an extension")
-  @commands.is_owner()
-  async def unload(self, inter: disnake.ApplicationCommandInteraction, path: str):
-    """ Unload an extension """
-    try:
-      self.bot.unload_extension(path)
-      await inter.response.send_message(content=f"{path} was unloaded.", ephemeral=True)
-    except:
-      await inter.response.send_message(content=f"Extension \"{path}\" not loaded or was not able to be found.", ephemeral=True)
+    @commands.slash_command()
+    @commands.is_owner()
+    async def guild_count(self, inter : disnake.ApplicationCommandInteraction):
+        """ Get the guild count of the bot """
+        await inter.response.send_message(content=f"{self.bot.user} is in {len(self.bot.guilds)} guilds.", ephemeral=True)
 
-  @commands.slash_command(description="Load an extension")
-  @commands.is_owner()
-  async def load(self, inter: disnake.ApplicationCommandInteraction, path: str):
-    """ Load an extension """
-    try:
-      self.bot.load_extension(path)
-      await inter.response.send_message(content=f"{path} was loaded.", ephemeral=True)
-    except:
-     await inter.response.send_message(content=f"Extension \"{path}\" already loaded or was not able to be found.", ephemeral=True)
+    @commands.slash_command(description="Load an extension")
+    @commands.is_owner()
+    async def load(self, inter: disnake.ApplicationCommandInteraction, path: str):
+        """ Load an extension """
+        try:
+          self.bot.load_extension(path)
+          await inter.response.send_message(content=f"{path} was loaded.", ephemeral=True)
+        except:
+          await inter.response.send_message(content=f"Extension \"{path}\" already loaded or was not able to be found.", ephemeral=True)
 
-  @commands.slash_command(description="Reload an extension")
-  @commands.is_owner()
-  async def reload(self, inter : disnake.ApplicationCommandInteraction, path : str):
-    """ Reload an extension """
-    try:
-      self.bot.reload_extension(path)
-      await inter.response.send_message(content=f"{path} was reloaded.", ephemeral=True)
-    except:
-      try:
-        self.bot.load_extension(path)
-        await inter.response.send_message(content=f"{path} was not loaded, but now is.", ephemeral=True)
-      except:
-        await inter.response.send_message(content=f"{path} was not able to be reloaded.", ephemeral=True)
+    @commands.slash_command(description="Unload an extension")
+    @commands.is_owner()
+    async def unload(self, inter: disnake.ApplicationCommandInteraction, path: str):
+        """ Unload an extension """
+        try:
+            self.bot.unload_extension(path)
+            await inter.response.send_message(content=f"{path} was unloaded.", ephemeral=True)
+        except:
+            await inter.response.send_message(content=f"Extension \"{path}\" not loaded or was not able to be found.", ephemeral=True)
 
-  @commands.slash_command(description="Clear n messages")
-  @commands.has_permissions(administrator=True)
-  async def wipe(self, inter: disnake.ApplicationCommandInteraction, n: int):
-    """ Delete 'n' messages """
-    await inter.channel.purge(limit=n)
-    await inter.response.send_message(content=f"{n} messages deleted.", ephemeral=True)
+    @commands.slash_command(description="Reload an extension")
+    @commands.is_owner()
+    async def reload(self, inter : disnake.ApplicationCommandInteraction, path : str):
+        """ Reload an extension """
+        try:
+          self.bot.reload_extension(path)
+          await inter.response.send_message(content=f"{path} was reloaded.", ephemeral=True)
+        except:
+          try:
+            self.bot.load_extension(path)
+            await inter.response.send_message(content=f"{path} was not loaded, but now is.", ephemeral=True)
+          except:
+            await inter.response.send_message(content=f"{path} was not able to be reloaded.", ephemeral=True)
+
+    @commands.slash_command(description="Clear n messages")
+    @commands.has_permissions(administrator=True)
+    async def wipe(self, inter: disnake.ApplicationCommandInteraction, n: int):
+        """ Delete 'n' messages """
+        await inter.channel.purge(limit=n)
+        await inter.response.send_message(content=f"{n} messages deleted.", ephemeral=True)
 
 def setup(client):
   client.add_cog(SlashCommands(client))
