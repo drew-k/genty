34dea3934bac1f9a8e15c6717a8f5a230813bf9e
TheKinster1 <66277127+TheKinster1@users.noreply.github.com>
2022-03-31 22:37:38 -0400

    Updated music extension

--- cogs/music.py
+++ cogs/music.py
@@ -2,9 +2,15 @@ import disnake
 from disnake.ext import commands
 import youtube_dl
 
+
 class MusicPlayer(commands.Cog):
-  def __init__(self, client):
-    self.client = client
+    def __init__(self, client):
+        self.client = client
+
+    @commands.slash_command(description="Play a YouTube video in a voice channel")
+    async def play(self, inter : disnake.ApplicationCommandInteraction, url : str):
+        if inter.author.voice is not None:
+            pass
 
 def setup(client):
   client.add_cog(MusicPlayer(client))
