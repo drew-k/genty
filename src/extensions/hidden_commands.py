85c3b7a2ba88ec2dca40a58c8a035d89b98fa8a1
TheKinster1 <66277127+TheKinster1@users.noreply.github.com>
2022-04-07 16:48:18 -0400

    Change response message to a user DM

--- src/extensions/hidden_commands.py
+++ src/extensions/hidden_commands.py
@@ -3,7 +3,7 @@ from disnake.ext import commands
 
 
 class HiddenCommands(commands.Cog):
-    """ Set up basic slash commands """
+    """ Set up hidden prefix commands """
 
     def __init__(self, bot):
         self.bot:commands.Bot = bot
@@ -15,17 +15,17 @@ class HiddenCommands(commands.Cog):
         await ctx.message.delete()
         try:
             self.bot.load_extension(path)
-            await ctx.send(content=f"{path} was loaded.", delete_after=10)
+            await ctx.author.send(content=f"{path} was loaded.")
         except (commands.NoEntryPointError, commands.ExtensionNotFound):
-            await ctx.send(content=f"Could not find an extension with name {path}.", delete_after=10)
+            await ctx.author.send(content=f"Could not find an extension with name {path}.")
         except commands.ExtensionAlreadyLoaded:
             try:
                 self.bot.reload_extension(path)
-                await ctx.send(content=f"{path} was reloaded.", delete_after=10)
+                await ctx.author.send(content=f"{path} was reloaded.")
             except commands.ExtensionFailed:
-                await ctx.send(content="Something went wrong.", delete_after=10)
+                await ctx.author.send(content="Something went wrong.")
         except commands.ExtensionFailed:
-            await ctx.send(content="Something went wrong.", delete_after=10)
+            await ctx.author.send(content="Something went wrong.")
 
     @commands.command(description="Unload an extension", hidden=True)
     @commands.is_owner()
@@ -34,11 +34,11 @@ class HiddenCommands(commands.Cog):
         await ctx.message.delete()
         try:
             self.bot.unload_extension(path)
-            await ctx.send(content=f"{path} was unloaded.", delete_after=10)
+            await ctx.author.send(content=f"{path} was unloaded.")
         except commands.ExtensionNotFound:
-            await ctx.send(contant=f"Could not find an extension with name {path}.", delete_after=10)
+            await ctx.author.send(contant=f"Could not find an extension with name {path}.")
         except commands.ExtensionNotLoaded:
-            await ctx.send(content=f"{path} was not loaded.", delete_after=10)
+            await ctx.author.send(content=f"{path} was not loaded.")
 
 def setup(client):
     client.add_cog(HiddenCommands(client))
